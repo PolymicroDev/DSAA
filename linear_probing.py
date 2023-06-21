3a2256692735d7990e1aa9bacbddbbe9e1b810fe
PolyMicroDev <hajdu.dani066@gmail.com>
2023-06-21 07:01:52 -0400

    This task sucks

---
+++ linear_probing.py
@@ -0,0 +1,55 @@
+class Hash_Table:
+    def __init__(self):
+        self.MAX = 10
+        self.arr =  [None for i in range(self.MAX)]
+    
+    def get_hash(self,key):
+        h = 0
+        for i in key:
+            h += ord(i)
+        return h%self.MAX
+    
+    def __setitem__(self,key,value):
+        h = self.get_hash(key)
+        if self.arr[h]:
+            while self.arr[h]:
+                if h < 9:
+                    h+=1
+                    continue
+                h = 0
+                while self.arr[h]:
+                    h+=1
+                    
+            self.arr[h] = value
+        self.arr[h] = value
+
+
+    def __getitem__(self,key):
+        h = self.get_hash(key)
+        
+        
+    
+    def __delitem__(self,key):
+        h = self.get_hash(key)
+    
+        
+    
+    
+
+table = Hash_Table()
+table['march 1'] = 130
+table['march 2'] = 120
+table['march 2'] = 110
+table['march 2'] = 111
+table['march 2'] = 112
+table['march 3'] = 113
+table['march 3'] = 114
+table['march 3'] = 115
+print(table.arr)
+
+
+    
+
+
+
+        
---
+++ linear_probing2.py
@@ -0,0 +1,50 @@
+class Hash_Table:
+    def __init__(self):
+        self.MAX = 10
+        self.arr =  [None for i in range(self.MAX)]
+    
+    def get_hash(self,key):
+        h = 0
+        for i in key:
+            h += ord(i)
+        return h%self.MAX
+    
+    def __setitem__(self,key,value):
+        h = self.get_hash(key)
+        if self.arr[h]:
+            if self.arr[h+1]:
+                i=0
+                while self.arr[i]:
+                    i+=1
+                self.arr[i] = value
+            else:
+                self.arr[h+1] = value
+        else:
+            self.arr[h] = value
+
+
+    def __getitem__(self,key):
+        h = self.get_hash(key)
+        
+        
+    
+    def __delitem__(self,key):
+        h = self.get_hash(key)
+    
+        
+    
+    
+
+table = Hash_Table()
+table['march 1'] = 130
+table['march 2'] = 120
+table['march 2'] = 110
+table['march 2'] = 111
+print(table.arr)
+
+
+    
+
+
+
+        
