4979d428d2c1fa0bc32f615de79c6c8ac50ad9db
ArchLinuxPolyMicroDev <hajdu.dani066@gmail.com>
2023-06-29 06:14:30 +0000

    Compliment search task solved

---
+++ compliment_search.py
@@ -0,0 +1,20 @@
+def find_compliment(numbers,sum):
+    compliments = {}
+
+    for i in range(len(numbers)):
+        compliments[numbers[i]] = i
+
+    for i in range(len(numbers)):
+        dif = sum-numbers[i]
+
+        if compliments.get(dif):
+            second_index = compliments[dif]
+            return print(f"Indices are: {i} and {second_index}")
+            
+    return ("No compliment was found")
+
+find_compliment([1,2,3,4,5,6,7],6)
+
+
+
+
