e5a9111e7b1fead70481b00ea6e4e3c284f8c642
PolyMicroDevWin <hajdu.dani066@gmail.com>
2023-07-14 17:49:23 +0200

    Merge_sort

--- binary_seach.py
+++ binary_seach.py
@@ -11,7 +11,6 @@ def linear_search(numbers_list, number_to_find):
 def binary_search(numbers_list, number_to_find):
     left_index = 0
     right_index = len(numbers_list) - 1
-    indices = []
 
     while left_index <= right_index:
         mid_index = (left_index + right_index) // 2
--- graphs.py
+++ graphs.py
@@ -1,20 +1,20 @@
 class Graph:
-    def __init__(self,edges):
+    def __init__(self, edges):
         self.edges = edges
         self.graph_dict = {}
-        for start,end in self.edges:
+        for start, end in self.edges:
             if start in self.graph_dict:
                 self.graph_dict[start].append(end)
             else:
                 self.graph_dict[start] = [end]
         print("Graph dictionary:", self.graph_dict)
 
-    def get_paths(self,start, end, path = []):
+    def get_paths(self, start, end, path=[]):
         path = path + [start]
         if start == end:
             return[path]
 
-        if start not in self.agraph_dict:
+        if start not in self.graph_dict:
             return []
 
         paths = []
@@ -23,7 +23,29 @@ class Graph:
                 new_paths = self.get_paths(node, end, path)
                 for p in new_paths:
                     paths.append(p)
+
         return paths
+
+    def get_shortest_path(self, start, end, path = []):
+        path = path + [start]
+
+        if start == end:
+            return path
+
+        if start not in self.graph_dict:
+            return None
+
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node,end,path)
+                if sp:
+                    if shortest_path is None or len(sp) < len(shortest_path):
+                        shortest_path = sp
+
+        return shortest_path
+
+
 if __name__ == '__main__':
     routes = [
         ("Mumbai", "Paris"),
@@ -34,9 +56,13 @@ if __name__ == '__main__':
         ("New York", "Toronto")
     ]
 
-route_graph = Graph(routes)
+    route_graph = Graph(routes)
 
-d = {
-    "Mumbai": ["Paris","Dubai"],
-    "Paris": ["Dubai","New York"],
-}
+    d = {
+        "Mumbai": ["Paris", "Dubai"],
+        "Paris": ["Dubai", "New York"],
+    }
+    print(route_graph.get_paths("Mumbai", "Toronto", []))
+
+
+
